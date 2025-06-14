924e2544d7e5fcbe5ad943c4e689d4b0edaa043b
NotArandomGUY <01game01play@gmail.com>
2025-06-14 12:39:43 +0800

    feat: catch up to live head using playback rate

--- src/extension/script/site/youtube/module/player.ts
+++ src/extension/script/site/youtube/module/player.ts
@@ -4,7 +4,50 @@ import Logger from '@ext/lib/logger'
 
 const LOGGER_OVERRIDE_ID = `logovr-${Date.now()}`
 
-function overridePlayerLogger(instance: object): void {
+const STAT_METHOD_MAP = {
+  bufferhealth: 'getBufferHealth',
+  livelatency: 'getLiveLatency'
+} satisfies Record<string, keyof YTVideoPlayer>
+const LATENCY_TARGET_STEPS = [60, 30, 25, 20, 15, 10, 7.5, 7, 6, 5, 4, 3, 2.5]
+const MIN_LATENCY_TARGET = LATENCY_TARGET_STEPS[LATENCY_TARGET_STEPS.length - 1]
+const MAX_LATENCY_TARGET = LATENCY_TARGET_STEPS[0]
+const BUFFER_HEALTH_THRESHOLD = 1.25
+const MIN_SYNC_RATE = 0.95
+const MAX_SYNC_RATE = 1.05
+const AVG_SAMPLE_SIZE = 10
+
+for (let target = 2; target >= 0.5; target -= 0.25) {
+  LATENCY_TARGET_STEPS.push(target)
+}
+
+interface YTVideoPlayer {
+  loop: boolean
+  playbackRate: number
+  videoData: object
+
+  getBufferHealth?(): number
+  getLiveLatency?(): number
+  getPreferredQuality?(): string
+  isAtLiveHead?(): boolean
+  isBackground?(): boolean
+  isFullscreen?(): boolean
+  isGapless?(): boolean
+  isPlaying?(): number
+  playVideo?(): void
+  sendAbandonmentPing?(): void
+  setMediaElement?(element: HTMLElement): void
+  setPlaybackRate?(rate: number): void
+}
+
+let moduleCtor: Function | null = null
+let player: YTVideoPlayer | null = null
+
+let lastSyncLiveHeadTime = 0
+let syncLiveHeadDeltaTime = 0
+let averageHealth = 0
+let averageLatency = 0
+
+function onLoggerConstruct(instance: object): void {
   const proto = Object.getPrototypeOf(instance)
 
   if (proto[LOGGER_OVERRIDE_ID]) return
@@ -26,38 +69,115 @@ function overridePlayerLogger(instance: object): void {
   })
 }
 
+function onAppModuleConstruct(_instance: object): void {
+  return
+}
+
+function onVideoPlayerModuleConstruct(instance: object): void {
+  player = instance as YTVideoPlayer
+
+  for (const prop of Object.values(instance)) {
+    if (prop == null || typeof prop !== 'object') continue
+
+    for (const key in prop) {
+      const value = prop[key]
+      if (value == null || !(value instanceof Map)) continue
+
+      for (const stat in STAT_METHOD_MAP) {
+        if (!value.has(stat)) continue
+
+        player[STAT_METHOD_MAP[stat as keyof typeof STAT_METHOD_MAP]] = value.get(stat)
+      }
+    }
+  }
+}
+
+function onModuleConstruct(instance: object): boolean {
+  Object.defineProperty(instance, 'logger', {
+    configurable: true,
+    set(logger) {
+      onLoggerConstruct(logger)
+      Object.defineProperty(instance, 'logger', {
+        configurable: true,
+        writable: true,
+        value: logger
+      })
+
+      setTimeout(() => {
+        switch (logger?.tag) {
+          case 'App':
+            onAppModuleConstruct(instance)
+            break
+          case 'VideoPlayer':
+            onVideoPlayerModuleConstruct(instance)
+            break
+        }
+      }, 1)
+    }
+  })
+
+  return true
+}
+
+function syncLiveHeadUpdate(): void {
+  const now = Date.now()
+  const delta = Math.max(1, now - lastSyncLiveHeadTime)
+
+  syncLiveHeadDeltaTime = ((syncLiveHeadDeltaTime * (AVG_SAMPLE_SIZE - 1)) + delta) / AVG_SAMPLE_SIZE
+  lastSyncLiveHeadTime = now
+
+  if (player == null || !player.isAtLiveHead?.() || !player.isPlaying?.()) return
+
+  const currentHealth = Number(player.getBufferHealth?.())
+  const currentLatency = Number(player.getLiveLatency?.())
+  if (isNaN(currentHealth) || isNaN(currentLatency)) return
+
+  averageHealth = ((averageHealth * (AVG_SAMPLE_SIZE - 1)) + currentHealth) / AVG_SAMPLE_SIZE
+  averageLatency = ((averageLatency * (AVG_SAMPLE_SIZE - 1)) + currentLatency) / AVG_SAMPLE_SIZE
+
+  const targetLatency = averageHealth > BUFFER_HEALTH_THRESHOLD ?
+    (LATENCY_TARGET_STEPS.find(target => averageLatency > target) ?? MIN_LATENCY_TARGET) :
+    (LATENCY_TARGET_STEPS.findLast(target => target >= averageLatency) ?? MAX_LATENCY_TARGET)
+  const latencyDeltaMs = (currentLatency - targetLatency) * 1e3
+  const playbackRate = Math.max(MIN_SYNC_RATE, Math.min(MAX_SYNC_RATE, (syncLiveHeadDeltaTime + latencyDeltaMs) / syncLiveHeadDeltaTime))
+
+  player.setPlaybackRate?.(playbackRate)
+}
+
 export default class YTPlayerModule extends Feature {
   protected activate(): boolean {
-    // Override player internal logger
+    // Override player internal modules
     Object.prototype.hasOwnProperty = new Hook(Object.prototype.hasOwnProperty).install(ctx => { // NOSONAR
       if (ctx.self instanceof HTMLDivElement && (ctx.self.id === 'player-api' || ctx.self.classList.contains('ytd-player'))) {
         Function.prototype.call = new Hook(Function.prototype.call).install(ctx => { // NOSONAR
-          const target = ctx.args[0]
-          const oldKeys = Object.keys(target).length
-          ctx.returnValue = ctx.origin.apply(ctx.self, ctx.args)
-          const newKeys = Object.keys(target).length
+          const fn = ctx.self
+          const instance = ctx.args[0]
 
-          if (oldKeys === newKeys) return HookResult.EXECUTION_CONTINUE
+          let result = HookResult.EXECUTION_IGNORE
+          if (moduleCtor == null) {
+            const oldKeys = Object.keys(instance).length
+            ctx.returnValue = ctx.origin.apply(fn, ctx.args)
+            const newKeys = Object.keys(instance).length
 
-          Object.defineProperty(target, 'logger', {
-            configurable: true,
-            set(v) {
-              overridePlayerLogger(v)
-              Object.defineProperty(target, 'logger', {
-                configurable: true,
-                writable: true,
-                value: v
-              })
-            },
-          })
+            if (oldKeys === newKeys) return HookResult.EXECUTION_CONTINUE
+
+            moduleCtor = fn
+            result = HookResult.EXECUTION_CONTINUE
+          }
+
+          if (fn !== moduleCtor || onModuleConstruct(instance)) return result
 
           Function.prototype.call = ctx.origin // NOSONAR
-          return HookResult.ACTION_UNINSTALL | HookResult.EXECUTION_CONTINUE
+          return HookResult.ACTION_UNINSTALL | result
         }).call
       }
 
       return HookResult.EXECUTION_IGNORE
     }).call
+
+    lastSyncLiveHeadTime = Date.now()
+    syncLiveHeadDeltaTime = 500
+    setInterval(syncLiveHeadUpdate, 500)
 
     return true
   }
