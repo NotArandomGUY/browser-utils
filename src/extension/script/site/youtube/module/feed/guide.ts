64a18579312481c3695f16b511cb2412dee8c7c8
NotArandomGUY <01game01play@gmail.com>
2025-07-09 22:02:34 +0800

    feat: auto refresh guide

--- src/extension/script/site/youtube/module/feed/guide.ts
+++ src/extension/script/site/youtube/module/feed/guide.ts
@@ -1,9 +1,13 @@
+import { floor, max, min } from '@ext/global/math'
 import { Feature } from '@ext/lib/feature'
 import { YTSignalActionType } from '@ext/site/youtube/api/endpoint'
 import { registerYTRendererPreProcessor, removeYTRendererPre, YTRenderer, YTRendererData, YTRendererSchemaMap } from '@ext/site/youtube/api/renderer'
 import { registerYTSignalActionHandler } from '@ext/site/youtube/module/core/action'
 import { isYTLoggedIn, registerYTPolymerCreateCallback } from '@ext/site/youtube/module/core/bootstrap'
 import { isShowShorts } from '@ext/site/youtube/module/feed/filter'
+
+const REFRESH_INTERVAL_SEC = 15 * 60
+const MIN_REFRESH_SEC = 30
 
 interface YTGuidePolymer {
   guidePromise?: object
@@ -15,6 +19,7 @@ interface YTGuidePolymer {
 }
 
 let guidePolymer: YTGuidePolymer | null = null
+let refreshTimer: ReturnType<typeof setTimeout> | null = null
 
 function filterGuideEntry(data: YTRendererData<YTRenderer<'guideEntryRenderer'>>): boolean {
   const browseId = data.navigationEndpoint?.browseEndpoint?.browseId ?? ''
@@ -30,13 +35,29 @@ function filterGuideEntry(data: YTRendererData<YTRenderer<'guideEntryRenderer'>>
 }
 
 function updateGuideResponse(data: YTRendererData<YTRenderer<'guideResponse'>>): boolean {
-  delete data.responseContext?.maxAgeSeconds
+  const { responseContext } = data
+
+  if (isYTLoggedIn()) {
+    const maxAgeSec = responseContext?.maxAgeSeconds ?? 0
+    const nextIntervalSec = (REFRESH_INTERVAL_SEC - (floor(Date.now() / 1e3) % REFRESH_INTERVAL_SEC))
+    const nextRefreshSec = max(MIN_REFRESH_SEC, min(maxAgeSec, nextIntervalSec))
+
+    if (refreshTimer != null) clearTimeout(refreshTimer)
+    refreshTimer = setTimeout(reloadYTGuide, nextRefreshSec * 1e3)
+  }
+
+  delete responseContext?.maxAgeSeconds
 
   return true
 }
 
 export async function reloadYTGuide(): Promise<void> {
   if (guidePolymer == null) return
+
+  if (refreshTimer != null) {
+    clearTimeout(refreshTimer)
+    refreshTimer = null
+  }
 
   delete guidePolymer.guidePromise
 
