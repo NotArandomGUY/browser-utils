5fd4dd62098c54cf4577c13dc322b28cc6ad8b1b
NotArandomGUY <01game01play@gmail.com>
2025-07-12 01:51:30 +0800

    feat: use update instead of remove

--- src/extension/worker/module/injector.ts
+++ src/extension/worker/module/injector.ts
@@ -23,61 +23,77 @@ function getScriptName(script: string): string {
   return script.replace(/[./]/g, '_')
 }
 
-function registerContentScripts(): void {
-  logger.info('registering content scripts...')
-
-  const excludeMatches = SITE_SCRIPT_CONFIG.filter(config => config.preventDefault).flatMap(site => site.matches ?? [])
-  const siteScripts = SITE_SCRIPT_CONFIG.map(config => {
-    const path = `js/site/${getScriptName(config.script)}.js`
-
-    return {
-      ...CONTENT_SCRIPT_CONFIG,
-      id: `main-${config.script}`,
-      matchOriginAsFallback: config.matches?.find(m => !MATCH_ORIGIN_REGEXP.test(m)) == null,
-      js: config.preventDefault ? [...RUNTIME_PATHS, path] : [path],
-      matches: config.matches
-    }
-  }) satisfies chrome.scripting.RegisteredContentScript[]
-
-  logger.debug('default exclude matches:', excludeMatches)
-  logger.debug('site content scripts:', siteScripts)
-
-  chrome.scripting.registerContentScripts([
-    {
-      ...CONTENT_SCRIPT_CONFIG,
-      id: 'isolated-preload',
-      matchOriginAsFallback: true,
-      js: ['js/preload/isolated.js'],
-      world: 'ISOLATED'
-    },
-    {
-      ...CONTENT_SCRIPT_CONFIG,
-      id: 'main-default',
-      matchOriginAsFallback: true,
-      excludeMatches,
-      js: [
-        ...RUNTIME_PATHS,
-        ...DEFAULT_SCRIPT_CONFIG.map(config => `js/default/${getScriptName(config.script)}.js`)
-      ]
-    },
-    ...siteScripts
-  ], () => {
-    chrome.scripting.getRegisteredContentScripts(scripts => logger.info('registered content scripts:', scripts))
-  })
-}
-
 export default class WorkerInjectorModule extends Feature {
   protected activate(): boolean {
-    chrome.scripting.getRegisteredContentScripts(scripts => {
-      chrome.scripting.unregisterContentScripts({ ids: scripts.map(script => script.id) }, registerContentScripts)
+    logger.info('registering content scripts...')
+
+    const excludeMatches = SITE_SCRIPT_CONFIG.filter(config => config.preventDefault).flatMap(site => site.matches ?? [])
+
+    logger.debug('exclude matches:', excludeMatches)
+
+    const scripts = [
+      {
+        ...CONTENT_SCRIPT_CONFIG,
+        id: 'isolated-preload',
+        matchOriginAsFallback: true,
+        js: ['js/preload/isolated.js'],
+        world: 'ISOLATED'
+      },
+      {
+        ...CONTENT_SCRIPT_CONFIG,
+        id: 'main-default',
+        matchOriginAsFallback: true,
+        excludeMatches,
+        js: [
+          ...RUNTIME_PATHS,
+          ...DEFAULT_SCRIPT_CONFIG.map(config => `js/default/${getScriptName(config.script)}.js`)
+        ]
+      },
+      ...SITE_SCRIPT_CONFIG.map(config => {
+        const path = `js/site/${getScriptName(config.script)}.js`
+
+        return {
+          ...CONTENT_SCRIPT_CONFIG,
+          id: `main-${config.script}`,
+          matchOriginAsFallback: config.matches?.find(m => !MATCH_ORIGIN_REGEXP.test(m)) == null,
+          js: config.preventDefault ? [...RUNTIME_PATHS, path] : [path],
+          matches: config.matches
+        }
+      })
+    ] satisfies chrome.scripting.RegisteredContentScript[]
+
+    chrome.scripting.getRegisteredContentScripts(registeredScripts => {
+      const registerScripts: chrome.scripting.RegisteredContentScript[] = []
+      const updateScripts: chrome.scripting.RegisteredContentScript[] = []
+      const unregisterScripts = registeredScripts.filter(s => scripts.find(({ id }) => id === s.id) == null)
+
+      for (const script of scripts) {
+        if (registeredScripts.find(({ id }) => id === script.id) == null) {
+          registerScripts.push(script)
+        } else {
+          updateScripts.push(script)
+        }
+      }
+
+      logger.debug('register/update/unregister content scripts:', registerScripts, updateScripts, unregisterScripts)
+
+      Promise.all([
+        registerScripts.length > 0 && chrome.scripting.registerContentScripts(registerScripts),
+        updateScripts.length > 0 && chrome.scripting.updateContentScripts(updateScripts),
+        unregisterScripts.length > 0 && chrome.scripting.unregisterContentScripts({ ids: unregisterScripts.map(s => s.id) })
+      ]).then(() => {
+        logger.info('updated content scripts')
+      }).catch(error => {
+        logger.error('update content scripts error:', error)
+      })
     })
 
     return true
   }
 
   protected deactivate(): boolean {
-    chrome.scripting.getRegisteredContentScripts(scripts => {
-      chrome.scripting.unregisterContentScripts({ ids: scripts.map(script => script.id) })
+    chrome.scripting.getRegisteredContentScripts(registeredScripts => {
+      chrome.scripting.unregisterContentScripts({ ids: registeredScripts.map(s => s.id) })
     })
 
     return true
