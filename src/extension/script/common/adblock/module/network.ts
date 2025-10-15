8165f0d651a1d3e15f9415ef93f4996ddbdf32ee
NotArandomGUY <01game01play@gmail.com>
2025-10-15 08:19:45 +0800

    feat: toggle static rules by network module state

--- src/extension/script/common/adblock/module/network.ts
+++ src/extension/script/common/adblock/module/network.ts
@@ -1,7 +1,11 @@
 import { Feature } from '@ext/lib/feature'
 import { NetworkContextState, NetworkState } from '@ext/lib/intercept/network'
 import { addInterceptNetworkUrlFilter, removeInterceptNetworkUrlFilter } from '@ext/lib/intercept/network/filter/url'
+import { ExtensionMessageSource, ExtensionMessageType, getExtensionMessageSender } from '@ext/lib/message/extension'
 import { buildHostnameRegexp } from '@ext/lib/regexp'
+import { MESSAGE_KEY } from '@virtual/package'
+
+const { sendMessageToWorker } = getExtensionMessageSender(MESSAGE_KEY, ExtensionMessageSource.MAIN)
 
 const FORBID_PATTERNS = [
   [buildHostnameRegexp(['adsrvr.org']), /./]
@@ -14,12 +18,16 @@ export default class AdblockNetworkModule extends Feature {
   }
 
   protected activate(): boolean {
+    sendMessageToWorker(ExtensionMessageType.PACKAGE_SCRIPT_NET_RULE, { scriptId: 'adblock', includeDomain: [location.host], excludeDomain: [] })
+
     for (const [hostPattern, pathPattern] of FORBID_PATTERNS) addInterceptNetworkUrlFilter(hostPattern, pathPattern, FORBID_CONTEXT_STATE)
 
     return true
   }
 
   protected deactivate(): boolean {
+    sendMessageToWorker(ExtensionMessageType.PACKAGE_SCRIPT_NET_RULE, { scriptId: 'adblock', includeDomain: [], excludeDomain: [location.host] })
+
     for (const [hostPattern] of FORBID_PATTERNS) removeInterceptNetworkUrlFilter(hostPattern)
 
     return true
--- src/extension/script/common/privacy/module/network.ts
+++ src/extension/script/common/privacy/module/network.ts
@@ -1,7 +1,11 @@
 import { Feature } from '@ext/lib/feature'
 import { NetworkContextState, NetworkState } from '@ext/lib/intercept/network'
 import { addInterceptNetworkUrlFilter, removeInterceptNetworkUrlFilter } from '@ext/lib/intercept/network/filter/url'
+import { ExtensionMessageSource, ExtensionMessageType, getExtensionMessageSender } from '@ext/lib/message/extension'
 import { buildHostnameRegexp } from '@ext/lib/regexp'
+import { MESSAGE_KEY } from '@virtual/package'
+
+const { sendMessageToWorker } = getExtensionMessageSender(MESSAGE_KEY, ExtensionMessageSource.MAIN)
 
 const FORBID_PATTERNS = [
   [buildHostnameRegexp(['licensing.bitmovin.com']), /./],
@@ -15,12 +19,16 @@ export default class PrivacyNetworkModule extends Feature {
   }
 
   protected activate(): boolean {
+    sendMessageToWorker(ExtensionMessageType.PACKAGE_SCRIPT_NET_RULE, { scriptId: 'privacy', includeDomain: [location.host], excludeDomain: [] })
+
     for (const [hostPattern, pathPattern] of FORBID_PATTERNS) addInterceptNetworkUrlFilter(hostPattern, pathPattern, FORBID_CONTEXT_STATE)
 
     return true
   }
 
   protected deactivate(): boolean {
+    sendMessageToWorker(ExtensionMessageType.PACKAGE_SCRIPT_NET_RULE, { scriptId: 'privacy', includeDomain: [], excludeDomain: [location.host] })
+
     for (const [hostPattern] of FORBID_PATTERNS) removeInterceptNetworkUrlFilter(hostPattern)
 
     return true
