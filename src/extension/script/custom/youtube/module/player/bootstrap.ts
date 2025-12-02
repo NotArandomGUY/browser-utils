61193e9990faa49dcaf8752c58236565afbfa148
NotArandomGUY <01game01play@gmail.com>
2025-12-03 05:25:06 +0800

    fix: temporary fix until they fix it officially

--- src/extension/script/custom/youtube/module/player/bootstrap.ts
+++ src/extension/script/custom/youtube/module/player/bootstrap.ts
@@ -8,6 +8,8 @@ import Logger from '@ext/lib/logger'
 const LOGGER_OVERRIDE_ID = `logovr-${Date.now()}`
 
 const STYLE_SHEET = [
+  // FIX: leanback animated overlay virtual list bug
+  '.app-quality-root .ytLrAnimatedOverlayHiding .ytLrAnimatedOverlayContainer{opacity:0!important;display:block!important}',
   // player stats
   ':root .ytp-sfn{margin:0;background:rgba(28,28,28,.5);border-radius:4px;color:#fff;position:absolute;left:1em;top:1em;z-index:64;min-width:26em;font-size:11px}',
   ':root .ytp-sfn .ytp-sfn-content{padding:5px}',
