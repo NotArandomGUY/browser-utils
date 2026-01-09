87d8e80237a2dd4f5816681b4345cfbfa1cd5253
NotArandomGUY <01game01play@gmail.com>
2026-01-09 19:09:11 +0800

    refactor: response filter helper

--- src/extension/script/custom/youtube/module/miscs/fixup.ts
+++ src/extension/script/custom/youtube/module/miscs/fixup.ts
@@ -1,4 +1,4 @@
-import { removeYTRendererPost, YTRenderer, YTRendererData, YTRendererSchemaMap } from '@ext/custom/youtube/api/renderer'
+import { removeYTRendererPost, YTRendererSchemaMap } from '@ext/custom/youtube/api/renderer'
 import { defineProperty, getOwnPropertyDescriptor, getOwnPropertyDescriptors, getPrototypeOf, keys } from '@ext/global/object'
 import { Feature } from '@ext/lib/feature'
 import InterceptDOM from '@ext/lib/intercept/dom'
@@ -6,22 +6,6 @@ import { HookResult } from '@ext/lib/intercept/hook'
 import Logger from '@ext/lib/logger'
 
 const logger = new Logger('YTMISCS-FIXUP')
-
-const filterContentContainer = (data: YTRendererData<YTRenderer<'richItemRenderer' | 'shelfRenderer'>>): boolean => {
-  return data.content != null && keys(data.content).length > 0
-}
-
-const filterContentsContainer = (data: YTRendererData<YTRenderer<'gridShelfViewModel'>>): boolean => {
-  return data.contents != null && data.contents.length > 0
-}
-
-const filterItemsContainer = (data: YTRendererData<YTRenderer<'guideSectionRenderer' | 'horizontalListRenderer' | 'reelShelfRenderer'>>): boolean => {
-  return data.items != null && data.items.length > 0
-}
-
-const filterShoppingOverlay = (data: YTRendererData<YTRenderer<'shoppingOverlayRenderer'>>): boolean => {
-  return data.productsData != null && data.productsData.length > 0
-}
 
 const stopControlEvent = (event: Event): void => {
   const { target } = event
@@ -31,19 +15,32 @@ const stopControlEvent = (event: Event): void => {
   }
 }
 
+const createArrayFilter = <K extends string>(key: K): (data: { [P in K]?: unknown[] }) => boolean => {
+  return data => Array.isArray(data[key]) && data[key].length > 0
+}
+
+const createObjectFilter = <K extends string>(key: K): (data: { [P in K]?: Record<string, unknown> }) => boolean => {
+  return data => data[key] != null && keys(data[key]).length > 0
+}
+
+const filterContent = createObjectFilter('content')
+const filterContents = createArrayFilter('contents')
+const filterItems = createArrayFilter('items')
+const filterProductsData = createArrayFilter('productsData')
+
 export default class YTMiscsFixupModule extends Feature {
   public constructor() {
     super('fixup')
   }
 
   protected activate(): boolean {
-    removeYTRendererPost(YTRendererSchemaMap['guideSectionRenderer'], filterItemsContainer)
-    removeYTRendererPost(YTRendererSchemaMap['gridShelfViewModel'], filterContentsContainer)
-    removeYTRendererPost(YTRendererSchemaMap['horizontalListRenderer'], filterItemsContainer)
-    removeYTRendererPost(YTRendererSchemaMap['reelShelfRenderer'], filterItemsContainer)
-    removeYTRendererPost(YTRendererSchemaMap['richItemRenderer'], filterContentContainer)
-    removeYTRendererPost(YTRendererSchemaMap['shelfRenderer'], filterContentContainer)
-    removeYTRendererPost(YTRendererSchemaMap['shoppingOverlayRenderer'], filterShoppingOverlay)
+    removeYTRendererPost(YTRendererSchemaMap['gridShelfViewModel'], filterContents)
+    removeYTRendererPost(YTRendererSchemaMap['guideSectionRenderer'], filterItems)
+    removeYTRendererPost(YTRendererSchemaMap['horizontalListRenderer'], filterItems)
+    removeYTRendererPost(YTRendererSchemaMap['reelShelfRenderer'], filterItems)
+    removeYTRendererPost(YTRendererSchemaMap['richItemRenderer'], filterContent)
+    removeYTRendererPost(YTRendererSchemaMap['shelfRenderer'], filterContent)
+    removeYTRendererPost(YTRendererSchemaMap['shoppingOverlayRenderer'], filterProductsData)
 
     InterceptDOM.setAppendChildCallback(ctx => {
       const node = ctx.args[0]
