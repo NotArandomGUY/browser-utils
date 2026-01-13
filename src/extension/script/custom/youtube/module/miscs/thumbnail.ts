e15245909c1d60c3d0c24608cfa7b9c97fe55be6
NotArandomGUY <01game01play@gmail.com>
2026-01-13 14:00:53 +0800

    fix: fallback to original when maxres thumbnail isn't available

--- src/extension/script/custom/youtube/module/miscs/thumbnail.ts
+++ src/extension/script/custom/youtube/module/miscs/thumbnail.ts
@@ -1,22 +1,59 @@
 import { registerYTValueProcessor } from '@ext/custom/youtube/api/processor'
 import { YTEndpoint, YTRenderer, YTValueData } from '@ext/custom/youtube/api/schema'
+import { defineProperty, getOwnPropertyDescriptor } from '@ext/global/object'
 import { Feature } from '@ext/lib/feature'
+import { preventDispatchEvent } from '@ext/lib/intercept/event'
+import { HookResult } from '@ext/lib/intercept/hook'
+import InterceptImage from '@ext/lib/intercept/image'
 
-const updateThumbnails = (data?: YTValueData<YTRenderer.Component<'thumbnail'>>): void => {
-  if (data?.thumbnails == null) return
+const { Image } = window
 
-  data.thumbnails = data.thumbnails.map(t => ({ ...t, url: t.url?.replace(/(?<=\/)hq(?=(default|\d)\.)/, 'maxres') }))
+const THUMBNAIL_TEST_TIMEOUT = 1e3
+const THUMBNAIL_404_WIDTH = 120
+const THUMBNAIL_URL_REGEXP = /\.ytimg\.com\/vi\//
+const CSS_STYLE_PROTO = CSSStyleDeclaration.prototype
+
+const getMaxResUrl = (url: string): string => {
+  return url.replace(/(?<=\/)hq(?=(default|\d)\.)/, 'maxres').replace(/\?.*$/, '')
 }
 
-const updateSetAppBackgroundCommand = (data: YTValueData<YTEndpoint.Mapped<'setAppBackgroundCommand'>>): boolean => {
-  updateThumbnails(data.image)
+const testThumbnail = (src: string) => new Promise<boolean>(resolve => {
+  const img = new Image()
+  const timer = setTimeout(() => img.src = '', THUMBNAIL_TEST_TIMEOUT)
 
+  img.onload = () => {
+    clearTimeout(timer)
+    resolve(img.naturalWidth !== THUMBNAIL_404_WIDTH)
+  }
+  img.onerror = img.onabort = () => {
+    clearTimeout(timer)
+    resolve(false)
+  }
+  img.src = src
+})
+
+const updateThumbnails = async (data?: YTValueData<YTRenderer.Component<'thumbnail'>>): Promise<void> => {
+  const thumbnails = data?.thumbnails
+  if (!Array.isArray(thumbnails)) return
+
+  for (const thumbnail of thumbnails) {
+    const { url } = thumbnail
+    if (url == null) continue
+
+    const maxresUrl = getMaxResUrl(url)
+    if (!await testThumbnail(maxresUrl)) continue
+
+    thumbnail.url = maxresUrl
+  }
+}
+
+const updateSetAppBackgroundCommand = async (data: YTValueData<YTEndpoint.Mapped<'setAppBackgroundCommand'>>): Promise<boolean> => {
+  await updateThumbnails(data.image)
   return true
 }
 
-const updateThumbnailRenderer = (data: YTValueData<YTRenderer.Mapped<'liveStreamOfflineSlateRenderer' | 'tileHeaderRenderer'>>): boolean => {
-  updateThumbnails(data.thumbnail)
-
+const updateLiveStreamOfflineSlateRenderer = async (data: YTValueData<YTRenderer.Mapped<'liveStreamOfflineSlateRenderer'>>): Promise<boolean> => {
+  await updateThumbnails(data.thumbnail)
   return true
 }
 
@@ -27,8 +64,44 @@ export default class YTMiscsThumbnailModule extends Feature {
 
   protected activate(): boolean {
     registerYTValueProcessor(YTEndpoint.mapped.setAppBackgroundCommand, updateSetAppBackgroundCommand)
-    registerYTValueProcessor(YTRenderer.mapped.liveStreamOfflineSlateRenderer, updateThumbnailRenderer)
-    registerYTValueProcessor(YTRenderer.mapped.tileHeaderRenderer, updateThumbnailRenderer)
+    registerYTValueProcessor(YTRenderer.mapped.liveStreamOfflineSlateRenderer, updateLiveStreamOfflineSlateRenderer)
+
+    const descriptor = getOwnPropertyDescriptor(CSS_STYLE_PROTO, 'cssText')
+    if (descriptor?.set != null) {
+      const setCssText = descriptor.set
+
+      defineProperty(CSS_STYLE_PROTO, 'cssText', {
+        ...descriptor,
+        set(cssText) {
+          if (typeof cssText !== 'string') return setCssText.call(this, cssText)
+
+          const originalUrl = /(?<=url\(.).*?(?=.\))/.exec(cssText)?.[0]
+          if (originalUrl == null || !THUMBNAIL_URL_REGEXP.test(originalUrl)) return setCssText.call(this, cssText)
+
+          const maxresUrl = getMaxResUrl(originalUrl)
+          testThumbnail(maxresUrl).then(success => {
+            if (success) cssText = cssText.replace(originalUrl, maxresUrl)
+            setCssText.call(this, cssText)
+          })
+        }
+      })
+    }
+
+    InterceptImage.setCallback((type, event) => {
+      if (type !== 'srcchange' || !event.cancelable) return
+
+      const originalUrl = String((<CustomEvent<string>>event).detail)
+      if (!THUMBNAIL_URL_REGEXP.test(originalUrl)) return
+
+      preventDispatchEvent(event)
+
+      const maxresUrl = getMaxResUrl(originalUrl)
+      testThumbnail(maxresUrl).then(success => {
+        event.target?.dispatchEvent(new CustomEvent('srcchange', { detail: success ? maxresUrl : originalUrl }))
+      })
+
+      return HookResult.EXECUTION_PASSTHROUGH
+    })
 
     return true
   }
