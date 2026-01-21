8496bd954ca0e3a5e7062995dccca13d43e7495e
NotArandomGUY <01game01play@gmail.com>
2026-01-22 00:26:27 +0800

    feat: force render all available emojis

--- src/extension/script/custom/youtube/module/chat/emoji-picker.ts
+++ src/extension/script/custom/youtube/module/chat/emoji-picker.ts
@@ -1,20 +1,56 @@
-import { registerYTValueProcessor } from '@ext/custom/youtube/api/processor'
+import { registerYTValueProcessor, YTValueProcessorType } from '@ext/custom/youtube/api/processor'
 import { YTRenderer, YTValueData } from '@ext/custom/youtube/api/schema'
 import { Feature } from '@ext/lib/feature'
 
+const titleCache = new Map<string, YTValueData<YTRenderer.Component<'text'>>>()
+const emojiCache = new Map<string, YTValueData<YTRenderer.Component<'emoji'>>[]>()
+
 const updateEmojiPickerRenderer = (data: YTValueData<YTRenderer.Mapped<'emojiPickerRenderer'>>): boolean => {
-  // Replace upsell categories with noraml categories
-  data.categories?.forEach(c => {
-    if (c.emojiPickerUpsellCategoryRenderer == null) return
+  const { categories } = data
 
-    const { categoryId, emojiIds, title } = c.emojiPickerUpsellCategoryRenderer
+  if (Array.isArray(categories)) {
+    for (const [categoryId, emojis] of emojiCache) {
+      if (categories.some(({ emojiPickerCategoryRenderer }) => emojiPickerCategoryRenderer?.categoryId === categoryId)) continue
 
-    c.emojiPickerCategoryRenderer = {
-      categoryId,
-      emojiIds,
-      title
+      categories.unshift({
+        emojiPickerCategoryRenderer: {
+          categoryId,
+          emojiIds: emojis.map(({ emojiId }) => emojiId).filter(emojiId => emojiId != null),
+          title: titleCache.get(categoryId) ?? { simpleText: 'Custom emojis' }
+        }
+      })
     }
-    delete c.emojiPickerUpsellCategoryRenderer
+  }
+
+  return true
+}
+
+const updateEmojiPickerUpsellCategoryRenderer = (data: YTValueData<YTRenderer.Mapped<'emojiPickerUpsellCategoryRenderer'>>): boolean => {
+  const { categoryId, title } = data
+
+  if (categoryId != null && title != null) {
+    titleCache.set(categoryId, title)
+  }
+
+  return false
+}
+
+const updateLiveChatRenderer = (data: YTValueData<YTRenderer.Mapped<'liveChatRenderer'>>): boolean => {
+  titleCache.clear()
+  emojiCache.clear()
+
+  data.emojis?.forEach(emoji => {
+    const categoryId = emoji.emojiId?.split('/')[0]
+    if (categoryId == null) return
+
+    let emojis = emojiCache.get(categoryId)
+    if (emojis == null) {
+      emojis = []
+      emojiCache.set(categoryId, emojis)
+    }
+    emojis.push(emoji)
+
+    delete emoji.isLocked
   })
 
   return true
@@ -26,7 +62,9 @@ export default class YTChatEmojiPickerModule extends Feature {
   }
 
   protected activate(): boolean {
-    registerYTValueProcessor(YTRenderer.mapped.emojiPickerRenderer, updateEmojiPickerRenderer)
+    registerYTValueProcessor(YTRenderer.mapped.emojiPickerUpsellCategoryRenderer, updateEmojiPickerUpsellCategoryRenderer)
+    registerYTValueProcessor(YTRenderer.mapped.liveChatRenderer, updateLiveChatRenderer)
+    registerYTValueProcessor(YTRenderer.mapped.emojiPickerRenderer, updateEmojiPickerRenderer, YTValueProcessorType.POST)
 
     return true
   }
